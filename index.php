d0f6964721cb2a5c9136d911afd0ef175d711893
Rohan04 <rohanchoubey04@gmail.com>
2020-11-28 01:52:24 +0530

    create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>HTML Meta Tag</title>
+      <meta http-equiv = "refresh" content = "1; url = /regis.php" />
+   </head>
+   <body>
+      <p>Sorry about that, let me take you to registeration</p>
+   </body>
+</html>
